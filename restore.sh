74b0782e4108134b326c36ca26783f9a34f6a3e8
yashuk803 <yashuk803@gmail.com>
2019-02-18 16:21:59 +0200

    Edit corrections in the conditions, comment, restore in other directore.

--- restore.sh
+++ restore.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
+set -e
 
-if [[ ! -n "$1" ]]; then
-    echo "expected second parameter";
+if [ -z $1 ] || [ "$#" -gt 1 ]; then
+    echo -e "expected one argument in format date d-m-Y";
+    exit 1;
 fi
 
-BACKUPS_ROOT=backups/$1;
-RESTORE_ROOT=restore/$1;
+REGEX_DATE='^[0-9]{2}-[0-9]{2}-[0-9]{4}$';
+
+if [[ ! $1 =~ $REGEX_DATE ]]; then
+    echo -e "Incorrect Date format";
+    exit 1;
+fi
+
+BACKUPS_ROOT=~/backups/$1;
+APP_ROOT=~/backend;
+APP_UPLOAD_ROOT=$APP_ROOT/web/uploads;
 
 if [[ ! -d $BACKUPS_ROOT ]]; then
-        echo "for $BACKUPS_ROOT/$1 no backup";
+        echo -e "for $BACKUPS_ROOT no backup";
+        exit 1;
 fi
 
-if [[ ! -d $RESTORE_ROOT ]]; then
-        echo "Creating restore directory";
-        mkdir -p $RESTORE_ROOT;
+if [[ ! -d $APP_UPLOAD_ROOT ]]; then
+        echo "Creating app directory";
+        mkdir -p $APP_UPLOAD_ROOT;
 fi
 
-#Копируем файлы в деректорию где нужно распоковать
-cp -i $BACKUPS_ROOT/dump.sql.gz  $RESTORE_ROOT;
+#Copy dump.sql to the root application directory
+cp -i $BACKUPS_ROOT/dump.sql.gz  $APP_ROOT;
 
-gzip -d  $RESTORE_ROOT/dump.sql.gz;
-unzip  $BACKUPS_ROOT/uploads.zip -d  $RESTORE_ROOT ;
+gzip -d  $APP_ROOT/dump.sql.gz;
+unzip  $BACKUPS_ROOT/uploads.zip -d  $APP_UPLOAD_ROOT ;
 
